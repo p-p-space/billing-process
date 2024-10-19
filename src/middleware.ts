bd2ba8b6bd7d7a80fb1cf3a8c88b72642af13b48
munsoenrique <enrique.penaloza.working@gmail.com>
2024-10-19 16:08:45 -0400

    Corrige error de seteo de cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,16 @@ import { Lang } from './interfaces';
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const lang = request.cookies.get(cookieLang);
+  const cookieValue = lang && langs.includes(lang.value as Lang) ? lang.value : defaultLang;
 
-  if (!lang || !langs.includes(lang.value as Lang)) {
-    response.cookies.set({
-      name: cookieLang,
-      value: defaultLang,
-      path: '/',
-      sameSite: 'lax',
-      httpOnly: true,
-      expires: Date.now() + 6 * 30 * 24 * 60 * 60 * 1000,
-    });
-  }
+  response.cookies.set({
+    name: cookieLang,
+    value: cookieValue,
+    path: '/',
+    sameSite: 'lax',
+    httpOnly: true,
+    expires: Date.now() + 6 * 30 * 24 * 60 * 60 * 1000,
+  });
 
   return response;
 }
