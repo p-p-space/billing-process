24e1bfbcd97f1f8ab2ddc5f0f36f67859b1f6423
munsoenrique <enrique.penaloza.working@gmail.com>
2024-10-19 11:03:07 -0400

    Traducciones del menu

--- src/components/navigation/sidebar/MenuList.tsx
+++ src/components/navigation/sidebar/MenuList.tsx
@@ -3,14 +3,16 @@
 import Image from 'next/image';
 //Internal app
 import logo from '%/images/logo.svg';
+import { useTranslations } from 'next-intl';
 
 export default function MenuList() {
+  const t = useTranslations();
   return (
     <>
       <div>
         <div className="flex items-start flex-col gap-6">
           <Image src={logo} alt="logo" width={176} height={40} priority />
-          <h2 className="text-base font-semibold">MENU</h2>
+          <h2 className="text-base font-semibold"> {t('App.menu')} </h2>
         </div>
 
         <nav className="my-4">
@@ -18,18 +20,20 @@ export default function MenuList() {
             href="#"
             className="flex gap-2 items-center py-1 px-4 rounded transition duration-200 hover:bg-primary-menu"
           >
-            <i className="ri-file-list-3-line text-2xl"></i> Purchase orders
+            <i className="ri-file-list-3-line text-2xl"></i>
+            {t('Menu.purchase-orders')}
           </a>
           <a
             href="#"
             className="flex gap-2 items-center py-1 px-4 rounded transition duration-200 hover:bg-primary-menu"
           >
-            <i className="ri-building-4-line text-2xl"></i>Companies
+            <i className="ri-building-4-line text-2xl"></i>
+            {t('Menu.companies')}
           </a>
         </nav>
 
         <div className="flex items-start flex-col gap-6">
-          <h2 className="text-base font-semibold">CONFIG & SUPPORT</h2>
+          <h2 className="text-base font-semibold"> {t('Menu.config-support')} </h2>
         </div>
 
         <nav className="my-4">
@@ -37,19 +41,22 @@ export default function MenuList() {
             href="#"
             className="flex gap-2 items-center py-1 px-4 rounded transition duration-200 hover:bg-primary-menu"
           >
-            <i className="ri-group-line text-2xl"></i>Users & Roles
+            <i className="ri-group-line text-2xl"></i>
+            {t('Menu.users-roles')}
           </a>
           <a
             href="#"
             className="flex gap-2 items-center py-1 px-4 rounded transition duration-200 hover:bg-primary-menu"
           >
-            <i className="ri-user-line text-2xl"></i>User profile
+            <i className="ri-user-line text-2xl"></i>
+            {t('Menu.user-profile')}
           </a>
           <a
             href="#"
             className="flex gap-2 items-center py-1 px-4 rounded transition duration-200 hover:bg-primary-menu"
           >
-            <i className="ri-shield-keyhole-line text-2xl"></i> Update password
+            <i className="ri-shield-keyhole-line text-2xl"></i>
+            {t('Menu.update-password')}
           </a>
         </nav>
       </div>
@@ -58,7 +65,7 @@ export default function MenuList() {
         <hr className="border-color-primary" />
         <a href="#" className="flex gap-2 items-center py-1 px-4 rounded transition duration-200 hover:bg-primary-menu">
           <i className="ri-logout-circle-r-line text-2xl"></i>
-          Log out
+          {t('Menu.log-out')}
         </a>
       </div>
     </>
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -10,6 +10,7 @@ export default getRequestConfig(async () => {
     ...(await import(`../../dictionary/${locale}/common.json`)).default,
     ...(await import(`../../dictionary/${locale}/dashboard.json`)).default,
     ...(await import(`../../dictionary/${locale}/head.json`)).default,
+    ...(await import(`../../dictionary/${locale}/menu.json`)).default,
     ...(await import(`../../dictionary/${locale}/password.json`)).default,
     ...(await import(`../../dictionary/${locale}/signin.json`)).default,
   };
