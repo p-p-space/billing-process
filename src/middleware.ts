904be488f7a3b0c962f5a99e6aadb536af37dbeb
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-29 21:12:50 -0400

    Ajuste de middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,25 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { cookieLang, langCookieName, availableValueCookie } from './i18n';
 
 export async function middleware(request: NextRequest) {
+  const { cookies, nextUrl, url } = request;
   const response = NextResponse.next();
-  const lang = request.cookies.get(langCookieName)?.value;
-  const cookieValue = await availableValueCookie(lang);
 
-  response.cookies.set({
-    ...cookieLang,
-    value: cookieValue,
-  });
+  if (nextUrl.pathname.startsWith('/api')) {
+    console.log(url);
+  } else {
+    const lang = cookies.get(langCookieName)?.value;
+    const cookieValue = await availableValueCookie(lang);
+
+    response.cookies.set({
+      ...cookieLang,
+      name: langCookieName,
+      value: cookieValue,
+    });
+  }
 
   return response;
 }
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|images|pwa|fonts|api).*)'],
+};
