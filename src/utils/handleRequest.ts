12136ab87a943e8a3dc3b6112ae477c8048c938e
mundoenrique <enrique.penaloza.working@gmail.com>
2024-12-14 13:19:14 -0400

    ajsuta menejo de errores

--- src/utils/handleRequest.ts
+++ src/utils/handleRequest.ts
@@ -19,45 +19,32 @@ import { importPKCS8 } from 'jose';
 export async function customerRequest(request: NextRequest) {
   const { method, nextUrl } = request;
   const { pathname, origin } = nextUrl;
-  let data;
-
-  try {
-    data = await request.json();
-  } catch (error) {
-    return NextResponse.json(
-      { code: '400.00.000', message: `Invalid json: ${(error as Error).message}` },
-      { status: 400 }
-    );
-  }
-
-  if (!data.payload) {
-    return NextResponse.json({ code: '400.00.001', message: 'Missing payload' }, { status: 400 });
-  }
-
   let decrypt;
+
   try {
+    const data = await request.json();
     const { payload } = data;
     const secJws = encode(SecretJws);
     const veriSig = await jwt.verifySignature(payload, secJws);
     const secJwe = await importPKCS8(jwePrivateKey, JweAlgRsa);
     decrypt = await jwt.decryptData(veriSig, secJwe);
-
     const jwtTemp = await createJWT(decrypt, jwsPrivateKey);
+
     await verifyJwt(jwtTemp, jwsPublicKey);
+
+    const dataRequest = {
+      formData: decrypt,
+      method,
+      url: `${origin}${pathname.replace('/v0/', '/v1/')}`,
+    } as DataRequest;
+
+    return await requestApi(dataRequest);
   } catch (error) {
     return NextResponse.json(
       { code: '500.00.000', message: `customerRequest: ${(error as Error).message}` },
       { status: 500 }
     );
   }
-
-  const dataRequest = {
-    formData: decrypt,
-    method,
-    url: `${origin}${pathname.replace('/v0/', '/v1/')}`,
-  } as DataRequest;
-
-  return await requestApi(dataRequest);
 }
 
 async function requestApi({ formData, method, url }: DataRequest) {
